eee779064a0e46b17ec176d4e6422ef75da28b2b
CaoKai <2655257336@qq.com>
2025-09-23 10:51:27 +0800

    移除登录页面表单及相关样式，简化页面结构

--- web/src/views/Login/Login.vue
+++ web/src/views/Login/Login.vue
@@ -3,21 +3,6 @@
 <template>
   <div class="Login">
     <h1 class="logo">Cao Kai's Blog</h1>
-    <div class="form">
-      <div class="title">后台管理入口</div>
-      <div class="desc">Edit / Publish Articles Online</div>
-      <div class="form">
-        <div class="account">
-          <input type="text" placeholder="请输入用户名" />
-        </div>
-        <div class="password">
-          <input type="password" placeholder="请输入密码" />
-        </div>
-        <div class="submit">
-          <button>登录</button>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
