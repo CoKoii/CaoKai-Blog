5d9f97ea117f0bd2f60dfe59f90367e85b5a9841
CK. <2655257336@qq.com>
2025-09-24 20:54:45 +0800

    更新 PWA 配置，移除开发模式设置以避免 Terser 压缩问题

--- web/config/index.ts
+++ web/config/index.ts
@@ -5,7 +5,6 @@ export const pwaConfig: Partial<VitePWAOptions> = {
   manifest: false,
   registerType: 'autoUpdate',
   workbox: {
-    mode: 'development', // 避免 Terser 压缩问题
     globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
   },
 }
