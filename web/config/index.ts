b03fd8836862d93aa978d9b556434f63a3e156b5
CK. <2655257336@qq.com>
2025-09-24 21:11:08 +0800

    更新 PWA 配置，移除 globPatterns 中的 woff2 文件类型，并将其添加到 runtimeCaching 中以优化字体缓存

--- web/config/index.ts
+++ web/config/index.ts
@@ -5,6 +5,19 @@ export const pwaConfig: Partial<VitePWAOptions> = {
   manifest: false,
   registerType: 'autoUpdate',
   workbox: {
-    globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+    globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+    runtimeCaching: [
+      {
+        urlPattern: /\.woff2$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'fonts-cache',
+          expiration: {
+            maxEntries: 100,
+            maxAgeSeconds: 60 * 60 * 24 * 365,
+          },
+        },
+      },
+    ],
   },
 }
